61291996a677a4b30e67396c3a0bf0711251e559
Ryang Sohn <loop.infinitely@gmail.com>
2023-10-08 15:02:06 +0900

    feat(hw4): add coding exercise solution

---
+++ hw4/SE05.R
@@ -0,0 +1,29 @@
+### SA05 Problem 1
+bino_dist <- dbinom(6:9, 29, 0.356)
+p <- sum(bino_dist)
+print(p)
+
+### SA05 Problem 2
+x <- qbinom(0.1, 29, 0.356)
+print(x)
+
+### SA05 Problem 3
+total_peaches <- 14400
+blemished_peaches <- 1100
+hyper_dist_exact <- dhyper(0:3, blemished_peaches, total_peaches - blemished_peaches, 55)
+p_exact = sum(hyper_dist_exact)
+print(p_exact)
+hyper_dist_approx <- dbinom(0:3, 55, blemished_peaches / total_peaches)
+p_approx = sum(hyper_dist_approx)
+print(p_approx)
+diff <- abs(p_exact - p_approx)
+print(diff)
+
+### SA05 Problem 4a
+negbino_dist <- dnbinom(0:7, 7, 0.29)
+p_negbino <- sum(negbino_dist)
+print(p_negbino)
+
+pois_dist <- dpois(0:4, 3.2)
+p_pois <- 1 - sum(pois_dist)
+print(p_pois)
